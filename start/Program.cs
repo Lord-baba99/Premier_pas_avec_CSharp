77a0e5878e5a511e1c2cc4a5a8d975a6bd9c7bc1
Lord-baba99 <prcl.baba@gmail.com>
2022-10-06 15:22:06 +0200

    Thirst

---
+++ start/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace start
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenu sur ma priemière app en c#");
+
+            Console.Write("Quel est ton nom ? ");
+            string name = Console.ReadLine();
+
+            Console.Write("Quel est votre age ? ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Hello, votre nom est " + name + ", vous avez " + age + "ans.");
+        }
+    }
+
+
+}
